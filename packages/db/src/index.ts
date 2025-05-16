2c2261b5c7f1f8821a9a21c36f5c80be8dc60a42
Jean P.D. Meijer <jean@analoginterface.io>
2025-05-16 21:01:09 +0200

    feat(db): generate auth database schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,8 +2,9 @@ import "server-only";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "@repo/env/server";
+import * as schema from "./schema";
 
 // Disable prefetch as it is not supported for Supabase "Transaction" pool mode
 const client = postgres(env.DATABASE_URL, { prepare: false });
 
-export const db = drizzle({ client });
+export const db = drizzle({ client, schema });
