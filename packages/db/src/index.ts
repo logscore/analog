6652dc9de8b15db281217fc78e9388f985966901
Jean P.D. Meijer <jean@analoginterface.io>
2025-05-16 19:18:00 +0200

    feat(db): add db package with drizzle

---
+++ packages/db/src/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { env } from "@repo/env/server";
+
+// Disable prefetch as it is not supported for Supabase "Transaction" pool mode
+const client = postgres(env.DATABASE_URL, { prepare: false });
+
+export const db = drizzle({ client });
--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -4,6 +4,7 @@ import { createEnv } from "@t3-oss/env-nextjs";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production", "test"]),
+    DATABASE_URL: z.string().url().startsWith("postgresql://"),
   },
   experimental__runtimeEnv: process.env,
   skipValidation: process.env.NODE_ENV !== "production",
