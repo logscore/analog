eec62e084a3fc31130f6f2b86f4b0e2cad37036d
Jean P.D. Meijer <jean@analoginterface.io>
2025-05-17 19:02:53 +0200

    fix(db): resolve issues db config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,8 @@
-import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema/index.ts",
+  schema: "./src/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
