3d15e99d9ea0133429f6d6e2c70c4d70cbf9f255
Jean P.D. Meijer <jean@analoginterface.io>
2025-05-19 20:39:31 +0200

    fix(auth): request correct oauth scopes and refresh token

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -4,6 +4,14 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@repo/db";
 import { env } from "@repo/env/server";
 
+export const GOOGLE_OAUTH_SCOPES = [
+  "email",
+  "profile",
+  "openid",
+  "https://mail.google.com/",
+  "https://www.googleapis.com/auth/calendar",
+];
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
@@ -12,6 +20,8 @@ export const auth = betterAuth({
     google: {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      scope: GOOGLE_OAUTH_SCOPES,
+      accessType: "offline",
     },
   },
 });
