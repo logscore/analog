6408516ff29b48794393595fdca5ed50a63459c9
Jean P.D. Meijer <9619050+JeanMeijer@users.noreply.github.com>
2025-05-21 10:44:31 +0200

    fix(web): resolve tRPC client not working (#21)

--- packages/env/src/client.ts
+++ packages/env/src/client.ts
@@ -1,7 +1,14 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
-  client: {},
-  runtimeEnv: {},
+  client: {
+    NEXT_PUBLIC_VERCEL_URL: z.string().optional(),
+    NEXT_PUBLIC_ENV: z.enum(["development", "test", "production"]),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    NEXT_PUBLIC_ENV: process.env.NODE_ENV,
+  },
   skipValidation: process.env.NODE_ENV !== "production",
 });
