8a042c44c1bb6b3ea9698c9f7c06e732bd8454ec
Jean P.D. Meijer <jean@analoginterface.io>
2025-05-21 14:04:40 +0200

    feat(api): add list calendar endpoint

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,13 +1,14 @@
-import "server-only";
 import {
   createCallerFactory,
   createTRPCContext,
   createTRPCRouter,
 } from "./trpc";
 import { userRouter } from "./routers/user";
+import { calendarsRouter } from "./routers/calendars";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
+  calendars: calendarsRouter,
 });
 
 export type AppRouter = typeof appRouter;
