5f81d8947bb97fd0e3efb50d41ea349fbef1f482
Logan Reeder <lsreedercontact@gmail.com>
2025-05-22 12:55:17 -0600

    Removed artifacts of landing page

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -6,5 +6,3 @@ import { TRPCReactProvider } from "@/lib/trpc/client";
 export function Providers(props: Readonly<{ children: ReactNode }>) {
   return <TRPCReactProvider>{props.children}</TRPCReactProvider>;
 }
-
-
